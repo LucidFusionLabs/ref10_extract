704667f3d1e63d634f618e8dfc68375e47ccece0
Trevor Perrin <unsafe@trevp.net>
2014-06-13 17:06:16 -0700

    Added main.c for ed25519.
    It compiles now.

---
+++ ed25519/main/main.c
@@ -0,0 +1,22 @@
+
+
+void randombytes(unsigned char *junk, unsigned long long junk2)
+{
+}
+
+int crypto_verify_32_ref(const unsigned char *junk, const unsigned char *junk2)
+{
+  return 0;
+}
+
+int crypto_hash_sha512_ref(unsigned char *junk ,const unsigned char *junk2,
+                           unsigned long long junk3)
+{
+  return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
