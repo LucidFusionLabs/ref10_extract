cd535a1885c5ca2c0903a717b23b37939ded9cf6
Trevor Perrin <unsafe@trevp.net>
2014-09-06 20:55:54 -0700

    Remove zeroization.

--- ed25519/additions/sha512.c
+++ ed25519/additions/sha512.c
@@ -9,6 +9,5 @@ int crypto_hash_sha512_ref(unsigned char *output ,const unsigned char *input,
   sph_sha512_init(&ctx);  
   sph_sha512(&ctx, input, len);
   sph_sha512_close(&ctx, output);
-  zeroize((unsigned char*)&ctx, sizeof(ctx));
   return 0;
 }
